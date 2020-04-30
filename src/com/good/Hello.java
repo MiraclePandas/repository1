514ba77f0f90e8a21b7e2428f66160bf46dd3c10
MiraclePandas <806743901@qq.com>
2020-04-30 15:53:28 +0800

    改动Hello.java文件中的主函数

--- src/com/good/Hello.java
+++ src/com/good/Hello.java
@@ -2,6 +2,9 @@ package com.good;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("我改了");
+        for(Integer i:new int[]{34,56,22}){
+            System.out.println(i);
+        }
     }
 }
